911b66e9e29ac0c7764bdfc6c8c484d2d0d8ca6a
Steve Korshakov <steve@korshakov.com>
2024-02-22 14:22:39 +0000

    feat: initial commit

---
+++ hubconv.py
@@ -0,0 +1,14 @@
+dependencies = ['torch', 'deep-phonemizer']
+
+def bigvsan():
+    import torch
+    from dp.phonemizer import Phonemizer
+    # Download
+    hub_dir = torch.hub.get_dir()
+    model_dir = os.path.join(hub_dir, 'checkpoints')
+    torch.hub.download_url_to_file("https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/DeepPhonemizer/en_us_cmudict_ipa_forward.pt")
+
+    # Load
+    model = Phonemizer.from_checkpoint(model_dir)
+    return model
+            
