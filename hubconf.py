c34e00625400daccba3f4d00eb882d61d490056f
Steve Korshakov <steve@korshakov.com>
2024-02-22 14:29:41 +0000

    wip: fixing dependencies

--- hubconf.py
+++ hubconf.py
@@ -4,11 +4,11 @@ def phonemizer():
     import torch
     import os
     from dp.phonemizer import Phonemizer
-    
+
     # Download
     hub_dir = torch.hub.get_dir()
-    model_dir = os.path.join(hub_dir, 'checkpoints')
-    torch.hub.download_url_to_file("https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/DeepPhonemizer/en_us_cmudict_ipa_forward.pt")
+    model_dir = os.path.join(hub_dir, 'checkpoints', 'phonemizer.pt')
+    torch.hub.download_url_to_file("https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/DeepPhonemizer/en_us_cmudict_ipa_forward.pt", model_dir)
 
     # Load
     model = Phonemizer.from_checkpoint(model_dir)
